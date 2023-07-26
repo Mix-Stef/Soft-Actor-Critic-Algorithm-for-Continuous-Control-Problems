d21503bba1219b1261a8e5ee18e578407ebf671e
Michael Stefanis <48247084+Mix-Stef@users.noreply.github.com>
2023-07-26 19:17:21 +0300

    Update replay_buffer.py

--- replay_buffer.py
+++ replay_buffer.py
@@ -4,23 +4,23 @@ import torch
 
 
 class ReplayBuffer:
-    def __init__(self, mem_size:int, state_dim:int, action_dim:int, batch_size:int) -> None:
+    def __init__(self, mem_size, num_states, num_actions, batch_size):
         self.mem_size = mem_size
-        self.state_dim = state_dim
-        self.action_dim = action_dim
+        self.num_states = num_states
+        self.num_actions = num_actions
         self.batch_size = batch_size
         assert self.mem_size > self.batch_size, "Batch size cannot be bigger than memory size."
-        self.state_mem = np.zeros(shape=(self.mem_size, self.state_dim))
-        self.action_mem = np.zeros(shape=(self.mem_size, self.action_dim))
+        self.state_mem = np.zeros(shape=(self.mem_size, self.num_states))
+        self.action_mem = np.zeros(shape=(self.mem_size, self.num_actions))
         self.reward_mem = np.zeros(shape=(self.mem_size, 1))
         self.next_state_mem = np.zeros_like(self.state_mem)
         self.terminated_mem = np.zeros(shape=(self.mem_size, 1))
         self.truncated_mem = np.zeros(shape=(self.mem_size, 1))
         self.mem_counter = 0
 
-    def memorize(self, state:List[float], action:List[float], next_state:List[float],
+    def memorize(self, state:List[float], action:float, next_state:List[float],
                  reward:float, term:bool, trunc:bool) -> None:
-        # Adds an experience to the memory. If the memory if full then return to the start and overwrite
+        # Adds an experience to the memory
         current_counter = self.mem_counter % self.mem_size
         self.state_mem[current_counter] = state
         self.action_mem[current_counter] = action
@@ -31,8 +31,7 @@ class ReplayBuffer:
         self.mem_counter += 1
 
     def sample_memories(self):
-        #Takes a random sample of experiences (batch_size experiences) to use in training. 
-        #Must also convert them to PyTorch tensors and send them to the GPU.
+        #Takes a sample of experiences to use in training
         indices = np.random.randint(low=0, high=self.mem_size, size=self.batch_size)
         states = torch.tensor(self.state_mem[indices, :],
                               dtype=torch.float32).to(device='cuda')
